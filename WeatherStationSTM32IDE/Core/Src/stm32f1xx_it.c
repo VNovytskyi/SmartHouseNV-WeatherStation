5624d5254a2255c421d6ac5902a7bd681bc5bdc8
VNovytskyi <vladyslavnovytskyi2000@gmail.com>
2020-02-10 13:26:49 +0200

    Disable LSE and HSE
    Disable LSE and HSE;
    Enable RTC global interrupt;
    Set System Clock Mux input PLLCLK;

--- WeatherStationSTM32IDE/Core/Src/stm32f1xx_it.c
+++ WeatherStationSTM32IDE/Core/Src/stm32f1xx_it.c
@@ -199,6 +199,20 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
+  * @brief This function handles RTC global interrupt.
+  */
+void RTC_IRQHandler(void)
+{
+  /* USER CODE BEGIN RTC_IRQn 0 */
+
+  /* USER CODE END RTC_IRQn 0 */
+  HAL_RTCEx_RTCIRQHandler(&hrtc);
+  /* USER CODE BEGIN RTC_IRQn 1 */
+
+  /* USER CODE END RTC_IRQn 1 */
+}
+
+/**
   * @brief This function handles EXTI line1 interrupt.
   */
 void EXTI1_IRQHandler(void)
